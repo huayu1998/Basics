13d05040f9afb512eac3b5994f082804319957e0
Huayu Liang <huayu98@vt.edu>
2024-01-23 17:12:22 -0500

    delete files

--- get_repo_info.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# GitHub API endpoint to get information about a repository
-repo_url="https://api.github.com/repos/huayu1998/Basics"
-
-# Make API request and save the output as a JSON file
-curl -s "$repo_url" -o "repo_info_huayu1998_Basics.json"
