2a84881f2df0c3dd4c4ca992850d37731d0b0b87
Huayu Liang <huayu98@vt.edu>
2024-01-23 17:45:04 -0500

    delete and update files

--- get_repo_issues.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# GitHub API endpoint to list issues in a repository
-issues_url="https://api.github.com/repos/huayu1998/Basics/issues"
-
-# Make API request and save the output as a JSON file
-curl -s "$issues_url" -o "repo_issues_huayu1998_Basics.json"
